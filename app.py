06e4bd8b0e3b315a75eecbcf0c3a84837ca42495
Henrijs Kravals <henrijskravals98@gmail.com>
2024-04-08 21:57:31 +0300

    web app with one static page

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, flash, redirect, render_template, request, session
+# from flask_session import Session
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- main.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
-
-app = Flask(__name__)
-
